ec2ac57ae087126ef764ddeff2c34672a458b727
Gabriel Schulze <gaschu95@gmail.com>
2017-08-12 21:05:30 +0200

    added additional fields

--- sqlite_handler.py
+++ sqlite_handler.py
@@ -1,3 +1,5 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+
 import logging
 import os
 import sqlite3
@@ -7,32 +9,43 @@ class SQLiteHandler(logging.Handler): # Inherit from logging.Handler
     """
     Logging handler that write logs to SQLite DB
     """
-    def __init__(self, filename):
+    def __init__(self, filename=None, reset=False):
         # run the regular Handler __init__
         logging.Handler.__init__(self)
         # Our custom argument
+        if filename == None:
+            filename = os.path.basename(__file__ + '.log.sqlite3db')
         self.filename = filename
-        db = sqlite3.connect(self.filename) # might need to use self.filename
-        db.execute("CREATE TABLE IF NOT EXISTS debug(date datetime, loggername text, filename, srclineno integer, func text, level text, msg text)")
+        db = sqlite3.connect(self.filename)
+        # collumn names from https://docs.python.org/3/library/logging.html#logrecord-attributes
+        db.execute(
+            'CREATE TABLE IF NOT EXISTS debug(args text, \
+            created text, exc_info text, filename text, funcName text, \
+            levelname text, levelno text, lineno text, module text, msecs text, \
+            msg text, name text, pathname text, process text, processName text, \
+            relativeCreated text, stack_info text, thread text, threadName text)')
         db.commit()
         db.close()
 
     def emit(self, record):
         # record.message is the log message
         db = sqlite3.connect(self.filename)
-        thisdate = datetime.now()
+
+        # convert all record values into strings (using a copy of record.__dict__)
+        recordDict = record.__dict__
+        for key in recordDict.keys():
+            recordDict[key] = str(recordDict[key])
+            # print(key + '\t\t' + recordDict[key])
+
         db.execute(
-            'INSERT INTO debug(date, loggername, filename, srclineno, func, level, msg) VALUES(?,?,?,?,?,?,?)',
-            (
-                thisdate,
-                record.name,
-                os.path.abspath(record.filename),
-                record.lineno,
-                record.funcName,
-                record.levelname,
-                record.msg,
-            )
-        )
+            'INSERT INTO debug(args, created, exc_info, filename, \
+            funcName, levelname, levelno, lineno, module, msecs, msg, name, \
+            pathname, process, processName, relativeCreated, stack_info, \
+            thread, threadName) \
+            VALUES(:args, :created, :exc_info, :filename, \
+            :funcName, :levelname, :levelno, :lineno, :module, :msecs, :msg, :name, \
+            :pathname, :process, :processName, :relativeCreated, :stack_info, \
+            :thread, :threadName)', recordDict)
         db.commit()
         db.close()
 
@@ -40,7 +53,8 @@ if __name__ == '__main__':
     # Create a logging object (after configuring logging)
     logger = logging.getLogger('someLoggerNameLikeDebugOrWhatever')
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(SQLiteHandler('debugLog.sqlite'))
+    # logger.addHandler(SQLiteHandler('debugLog.sqlite3db'))
+    logger.addHandler(SQLiteHandler())
     logger.debug('Test 1')
-    logger.warning('Some warning')
-    logger.error('Alarma!')
+    # logger.warning('Some warning')
+    # logger.error('Alarma!')
